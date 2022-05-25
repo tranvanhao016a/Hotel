6ddd6b94c0e72bf0be19eaa2b45dec520512b7af
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-05-25 23:51:08 +0700

    Hao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }, { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) }, { path: 'rooms', loadChildren: () => import('./pages/rooms/rooms.module').then(m => m.RoomsModule) }, { path: 'roomdetail', loadChildren: () => import('./pages/roomdetail/roomdetail.module').then(m => m.RoomdetailModule) }];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/customs/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }, 
+{ path: 'contact', loadChildren: () => import('./pages/customs/contact/contact.module').then(m => m.ContactModule) },
+ { path: 'rooms', loadChildren: () => import('./pages/customs/rooms/rooms.module').then(m => m.RoomsModule) }, 
+ { path: 'roomdetail', loadChildren: () => import('./pages/customs/roomdetail/roomdetail.module').then(m => m.RoomdetailModule) },
+{ path: 'booking', loadChildren: () => import('./pages/customs/booking/booking.module').then(m => m.BookingModule) },
+{ path: 'customer', loadChildren: () => import('./pages/admin/customer/customer.module').then(m => m.CustomerModule) },
+{ path: 'staff', loadChildren: () => import('./pages/admin/staff/staff.module').then(m => m.StaffModule) },
+{ path: 'room', loadChildren: () => import('./pages/admin/room/room.module').then(m => m.RoomModule) },
+{ path: 'reservation', loadChildren: () => import('./pages/admin/reservation/reservation.module').then(m => m.ReservationModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
