2dafad81d56febb61a1ea1ba1cd08b4a850a1a6c
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-05-26 14:26:43 +0700

    Update repos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/customs/home/home.module').then(m => m.HomeModule) }, { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) }, 
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/customs/home/home.module').then(m => m.HomeModule) },
+ { path: 'about', loadChildren: () => import('./pages/customs/about/about.module').then(m => m.AboutModule) }, 
 { path: 'contact', loadChildren: () => import('./pages/customs/contact/contact.module').then(m => m.ContactModule) },
  { path: 'rooms', loadChildren: () => import('./pages/customs/rooms/rooms.module').then(m => m.RoomsModule) }, 
  { path: 'roomdetail', loadChildren: () => import('./pages/customs/roomdetail/roomdetail.module').then(m => m.RoomdetailModule) },
