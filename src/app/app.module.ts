592ea187eb18839944634901f77a239a8ede00b6
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-05-26 14:35:54 +0700

    Boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NbarComponent } from './components/nbar/nbar.component';
 import { FootherComponent } from './components/foother/foother.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FootherComponent } from './components/foother/foother.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
